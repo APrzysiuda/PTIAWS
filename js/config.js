ee1c27faf850d99b9a5d28010406170f18f6c162
APrzysiuda <56821340+APrzysiuda@users.noreply.github.com>
2021-05-03 10:54:40 +0200

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'eu-central-1_3dou5LmNH', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1o3lnhbb945abbc4a3t1i82jn', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-central-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
